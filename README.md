a370d81b13e0f6c2985cacaecf3bac91659d3baf
Frieder Hannenheim <frieder12.fml@pm.me>
2022-01-20 18:45:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vertretungsplan
+Dies ist ein kleines Script, welches den Vertretungsplan meiner Berufsschule abruft und ihn als Benachrichtigung darstellt. 
+## Installation
+    git clone https://github.com/FriederHannenheim/bsz_et_dd_vertretungsplan.git 
+    cd bsz_et_dd_vertretungsplan 
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env bash
+
+USER="$(whoami)"
+SCRIPT_DIR="/home/$USER/bin"
+SYSTEMD_DIR="/home/$USER/.config/systemd/user"
+
+read -p "Klasse: " klasse
+read -p "geschuetzt.bszet.de Nutzername: " nutzer
+read -p "geschuetzt.bszet.de Passwort: " passwort
+
+mkdir -p "$SCRIPT_DIR" "$SYSTEMD_DIR"
+
+sed "s/&0/$USER/g" vertretung.service > "$SYSTEMD_DIR/vertretung.service"
+cp vertretung.timer "$SYSTEMD_DIR"
+sed "s/&0/$klasse/g;s/&1/$nutzer:$passwort/g" check_vertretungsplan > "$SCRIPT_DIR/check_vertretungsplan"
+
+chmod +x "$SCRIPT_DIR/check_vertretungsplan"
+
+systemctl --user enable vertretung.timer
